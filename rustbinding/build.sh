843f7755c3ac4e4243f6e2f56f275781882ee7f6
tropicbliss <tropicbliss@protonmail.com>
2024-11-30 17:58:07 +0800

    wasip1

--- rustbinding/build.sh
+++ rustbinding/build.sh
@@ -1,2 +1,2 @@
-cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/rustbinding.wasm ../src/main/resources
+cargo build --target wasm32-wasip1 --release
+cp target/wasm32-wasip1/release/rustbinding.wasm ../src/main/resources
